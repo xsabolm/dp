dc53cd2079cdcc6e3726af5c7b93f5e5ab399167
Marcel Sabol <sabol.marcel@gmail.com>
2019-02-11 23:17:58 +0100

    init commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DP_WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            txtBoxOpenFile.Text = openFileDialog.FileName;
+        }
+
+        private void btnACKOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string contents = File.ReadAllText(txtBoxOpenFile.Text);
+            Console.Write(contents);
+        }
+    }
+}
